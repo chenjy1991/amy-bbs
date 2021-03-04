a90069eec5cd15162c3d1f5d9e51a42ba5e8b82a
ChenJY <chenjy@chenjy.cn>
2021-03-04 22:49:55 +0800

    first commit

---
+++ src/main/java/cn/chenjy/java/amybbs/AmyBbsApplication.java
@@ -0,0 +1,13 @@
+package cn.chenjy.java.amybbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmyBbsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmyBbsApplication.class, args);
+    }
+
+}
